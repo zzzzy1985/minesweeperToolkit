4a5d42bb77cec8cabd4ba4d4011f06c390687ffb
zzzzy1985 <zzzzy2013@gmail.com>
2014-01-07 17:16:58 +0800

    重构

--- mc97toolkit/src/com/minesweeperToolkit/videoUtil/VideoCommon.java
+++ mc97toolkit/src/com/minesweeperToolkit/videoUtil/VideoCommon.java
@@ -12,10 +12,10 @@ import com.minesweeperToolkit.bean.VideoDisplayBean;
 public class VideoCommon {
 	public VideoCheckBean checkVersion(byte[] byteStream ){
 		VideoCheckBean bean = new VideoCheckBean();
-		String videoType = new String(byteStream, 1,
+		/*String videoType = new String(byteStream, 1,
 				4);
 		bean.videoType = TYPE_RMV;
-		bean.videoVersion = VERSION2;
+		bean.videoVersion = VERSION2;*/
 		return bean;
 	}
 }
